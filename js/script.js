c5edc0c44f32330a86f40fa396d2f7a623fe32d5
BeatrizAlmeida07 <beatriz.9363846@aluno.mg.gov.br>
2026-05-06 14:16:37 +0000

    update atualizar tela

--- js/script.js
+++ js/script.js
@@ -1,30 +1,25 @@
-//=== ESTADO (DADOS DA APLICAÇÃO) === 
+//=== ESTADO (dados da aplicação) === 
 
 let likeCount = 0;
 let dislikeCount = 0;
 let curtido = false;
 let descurtido = false;
 
-//=== SERVICE (regras de negócio) ===
+//=== SERVICE (regras de negócio) === 
 
 function curtir() {
   if (curtido == false){
     likeCount++;
     curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
-
 
     if(descurtido == true){
       dislikeCount--;
       descurtido = false;
-      document.getElementById("dislikeCount").innerText = dislikeCount;
-
     }
 
   }else{
     likeCount--;
     curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
   }
 
 }
@@ -33,28 +28,34 @@ function descurtir() {
   if(descurtido == false){
     dislikeCount++;
     descurtido = true;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
 
     if(curtido == true){
       likeCount--;
       curtido = false;
-      document.getElementById("likeCount").innerText = likeCount;
     }
 
   }
   else{
     dislikeCount--;
     descurtido = false;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
   }
 }
-// === CONTROLER (intermediação) ===
+
+// === VIEW (interface/renderização)===
+function atualizarTela(){
+  document.getElementById("likeCount").innerText = likeCount;
+  document.getElementById("dislikeCount").innerText = dislikeCount;
+}
+
+//=== CONTROLLER (intermediação)===
 
 function clicarCurtir(){
   curtir();
+  atualizarTela();
 }
 function clicarDescurtir(){
   descurtir();
+  atualizarTela();
 }
 
 // === EVENTOS ===
@@ -62,5 +63,6 @@ function clicarDescurtir(){
 document.getElementById("likeBtn").addEventListener("click", clicarCurtir);
 document.getElementById("dislikeBtn").addEventListener("click", clicarDescurtir);
 
+// === INICIALIZAÇÃO ===  
 
-
+atualizarTela();
